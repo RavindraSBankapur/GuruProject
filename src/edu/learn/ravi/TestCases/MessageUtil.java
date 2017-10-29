940b1162118950e850391eb171c3209fd8766c2f
RavindraSBankapur <rsbravindra@gmail.com>
2017-10-29 21:04:10 +0530

    second commit to demonstrate changes pushed....

--- src/edu/learn/ravi/TestCases/MessageUtil.java
+++ src/edu/learn/ravi/TestCases/MessageUtil.java
@@ -1,6 +1,7 @@
 package edu.learn.ravi.TestCases;
 
 public class MessageUtil {
+	//testing the modifications done in master checkin to github
 	private static String message;
 	public MessageUtil(String message) {
 		System.out.println("MessageUtility Running..");
